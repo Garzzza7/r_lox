9050c835cbe4837c12f6e0875b3978df2429ee9b
Garzzza7 <piotr.czapela@gmail.com>
2024-06-06 22:14:34 +0200

    slight modifications

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,77 @@
-
 //use std::arch::x86_64::_mm512_scalef_round_pd;
 
-
-use std::{env, io};
-use std::io::prelude::*;
 use std::fs::File;
-static mut hadError: bool = false;
+use std::io::prelude::*;
+use std::{env, io};
 
+static mut HAD_ERROR: bool = false;
 
 mod scanner;
+
 fn main() {
     let args: Vec<_> = env::args().collect();
-    //println!("ROZMIAR = {}",args.len());
+    //println!("ROZMIAR = {}", args.len());
     if args.len() > 2 {
-        println!("Usage: rlox [script]");
-    }else if args.len() == 2 {
+        println!("Pass --help if you want to know what rlox is capable of.");
+        std::process::exit(64);
+    } else if args.len() == 2 {
         //println!("TUTAJ PATZRZ {}",args[1]);
-        let buffer = &args[1];
-        run_file(buffer);
-    }else{
+        /* let buffer = &args[1] */
+        run_file(&args[1]);
+    } else {
         run_prompt();
     }
 }
 
-fn run_file(_path : &String) -> () {
+fn run_file(_path: &String) -> () {
     let mut file = File::open(_path).expect("Unable to open the file");
     let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Unable to read the file");
-    //println!("GOWNO w plynie skurwysynie {}", contents);
+    file.read_to_string(&mut contents)
+        .expect("Unable to read the file");
+    //println!("{}", contents);
     run(&contents);
-    unsafe{
-        if hadError {
+    unsafe {
+        if HAD_ERROR {
             std::process::exit(0);
         }
     }
 }
 fn run_prompt() -> () {
-    loop{
-        print!("> ");
-        let stdin = io::stdin();
-        let mut line = String::with_capacity(2048);
+    let stdin = io::stdin();
+    let mut line = String::new();
+    loop {
+        print!("rlox> ");
         stdin.read_line(&mut line);
-        run(&line);
-        unsafe{
-            hadError=false;
+        if line.is_empty() == true {
+            break;
         }
+        run(&line);
         line.clear();
+        unsafe {
+            HAD_ERROR = false;
+        }
     }
 }
-fn run(_source : &String) -> () {
-    //println!("HELLO FROM RUN");
-
-    //let stream: proc_macro::TokenStream = _source.parse()
-    let s:String =_source.to_string();
-    let bruh = s.split(" ");
-    for i in bruh {
-        println!("{}",i);
+fn run(_source: &String) -> () {
+    unsafe {
+        if HAD_ERROR == true {
+            std::process::exit(65);
+        }
+    }
+    let token = _source.split_ascii_whitespace();
+    for i in token {
+        println!("{}", i);
     }
 }
 
-fn error(line : i32, message: &String) -> (){
-    let s: String = "".to_string();
-    report(line, &s, &message);
+fn error(line: i32, message: &String) -> () {
+    let wher: String = String::from("");
+    report(line, &wher, &message);
 }
-fn report(line: i32, w: &String , message: &String) ->() {
-    println!("[line {} ] Error {} : {}",line,w,message);
-    unsafe{
-    hadError = true;
+
+fn report(line: i32, wher: &String, message: &String) -> () {
+    println!("[line {} ] Error {} : {}", line, wher, message);
+    unsafe {
+        HAD_ERROR = true;
     }
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,43 +1,105 @@
-use std::{fmt::Debug, f32::consts::E};
 use std::collections::HashMap;
-#[derive(Debug,Clone)]
+use std::fmt::{Debug, Display};
+
+#[derive(Debug, Clone)]
 pub enum TokenType {
     // Single-character tokens.
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
     // One or two character tokens.
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
     // Literals.
-    IDENTIFIER, STRING, NUMBER,
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
     // Keywords.
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-    EOF
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
 }
-#[derive(Debug,Clone)]
+
+#[derive(Debug, Clone)]
 pub enum Literal {
     Indentifier(String),
     Str(String),
     Number(f64),
 }
+
 #[derive(Clone)]
 pub struct Token {
     pub ttype: TokenType,
     pub lexeme: Vec<u8>,
     pub literal: Option<Literal>,
     pub line: usize,
-
 }
-impl Debug for Token{
+
+impl Default for Token {
+    fn default() -> Token {
+        Token {
+            ttype: TokenType::EOF,
+            lexeme: Vec::new(),
+            literal: None,
+            line: 0,
+        }
+    }
+}
+
+// impl Display for Option<Literal> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(f, "{}", &self)
+//     }
+// }
+//
+// impl Display for Token {
+//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(
+//             f,
+//             "Token{{ ttype = {}, lexeme = {}, literal = {}, line = {}}}",
+//             self.ttype, self.lexeme, self.literal, self.line
+//         )
+//     }
+// }
+
+impl Debug for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f,"Token{{ ttype = {:?}, lexeme = {:?}, literal = {:?}, line = {:?}}}",
-            self.ttype,
-            self.lexeme,
-            self.literal,
-            self.line
+        write!(
+            f,
+            "Token{{ ttype = {:?}, lexeme = {:?}, literal = {:?}, line = {:?}}}",
+            self.ttype, self.lexeme, self.literal, self.line
         )
     }
 }
@@ -48,12 +110,17 @@ struct Scanner {
     start: usize,
     current: usize,
     line: usize,
-    keywords:HashMap<String,TokenType>,
+    keywords: HashMap<String, TokenType>,
 }
-impl Default for Scanner{
+impl Default for Scanner {
     fn default() -> Scanner {
-        Scanner { source: Vec::new(), tokens: Vec::new(), start: 0, current: 0, line: 1 ,keywords:
-                vec![
+        Scanner {
+            source: Vec::new(),
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+            keywords: vec![
                 ("and", TokenType::AND),
                 ("class", TokenType::CLASS),
                 ("else", TokenType::ELSE),
@@ -74,120 +141,121 @@ impl Default for Scanner{
             .into_iter()
             .map(|(k, v)| (String::from(k), v))
             .collect(),
-
         }
     }
 }
+
 impl Scanner {
-    fn scanTokens(&mut self){
-        loop{
-            if !self.isAtEnd() {break;}
-            self.start=self.current;
-            self.scanToken();       
+    fn scan_tokens(&mut self) {
+        loop {
+            if !self.is_at_end() {
+                break;
+            }
+            self.start = self.current;
+            self.scan_token();
         }
-   //     let lit: &dyn Any=&"";
-       // let new_token: Token = Token { ttype: TokenType::EOF, lexeme: String::from(""), literal: lit, line: self.line };
-        self.tokens.push( Token { ttype: TokenType::EOF, lexeme: Vec::new(), literal: None, line: self.line });
+        //     let lit: &dyn Any=&"";
+        // let new_token: Token = Token { ttype: TokenType::EOF, lexeme: String::from(""), literal: lit, line: self.line };
+        self.tokens.push(Token {
+            ttype: TokenType::EOF,
+            lexeme: Vec::new(),
+            literal: None,
+            line: self.line,
+        });
         //self.tokens
         //let mut newvec: Vec<Token>=Vec::new();
         //newvec=self.tokens;
         //newvec
     }
-    fn isAtEnd(&mut self)->bool{
-        self.current>=self.source.len().try_into().unwrap()
+    fn is_at_end(&mut self) -> bool {
+        self.current >= self.source.len().try_into().unwrap()
     }
-    fn scanToken(&mut self) -> () {
-        let c:char=self.advance();
+    fn scan_token(&mut self) -> () {
+        let c: char = self.advance();
         match c {
-            '(' => self.addToken(TokenType::LEFT_PAREN),
-            ')' => self.addToken(TokenType::RIGHT_PAREN),
-            '{' => self.addToken(TokenType::LEFT_BRACE),
-            '}' => self.addToken(TokenType::RIGHT_BRACE),
-            ',' => self.addToken(TokenType::COMMA),
-            '.' => self.addToken(TokenType::DOT),
-            '-' => self.addToken(TokenType::MINUS),
-            '+' => self.addToken(TokenType::PLUS),
-            ';' => self.addToken(TokenType::SEMICOLON),
-            '*' => self.addToken(TokenType::STAR),
-            '!' =>{ 
-                    if self.matches('='){
-                    self.addToken(TokenType::BANG_EQUAL);
-                    }else{
-                        self.addToken(TokenType::BANG);
-                    } 
+            '(' => self.add_token(TokenType::LEFT_PAREN),
+            ')' => self.add_token(TokenType::RIGHT_PAREN),
+            '{' => self.add_token(TokenType::LEFT_BRACE),
+            '}' => self.add_token(TokenType::RIGHT_BRACE),
+            ',' => self.add_token(TokenType::COMMA),
+            '.' => self.add_token(TokenType::DOT),
+            '-' => self.add_token(TokenType::MINUS),
+            '+' => self.add_token(TokenType::PLUS),
+            ';' => self.add_token(TokenType::SEMICOLON),
+            '*' => self.add_token(TokenType::STAR),
+            '!' => {
+                if self.matches('=') {
+                    self.add_token(TokenType::BANG_EQUAL);
+                } else {
+                    self.add_token(TokenType::BANG);
                 }
-            '='=>{ 
-                    if self.matches('='){
-                    self.addToken(TokenType::EQUAL_EQUAL);
-                    }else{
-                        self.addToken(TokenType::EQUAL);
-                    } 
+            }
+            '=' => {
+                if self.matches('=') {
+                    self.add_token(TokenType::EQUAL_EQUAL);
+                } else {
+                    self.add_token(TokenType::EQUAL);
                 }
-            '<'=>{ 
-                    if self.matches('='){
-                    self.addToken(TokenType::LESS_EQUAL);
-                    }else{
-                        self.addToken(TokenType::LESS);
-                    } 
+            }
+            '<' => {
+                if self.matches('=') {
+                    self.add_token(TokenType::LESS_EQUAL);
+                } else {
+                    self.add_token(TokenType::LESS);
                 }
-            '>'=>{ 
-                    if self.matches('='){
-                    self.addToken(TokenType::GREATER_EQUAL);
-                    }else{
-                        self.addToken(TokenType::GREATER);
-                    } 
+            }
+            '>' => {
+                if self.matches('=') {
+                    self.add_token(TokenType::GREATER_EQUAL);
+                } else {
+                    self.add_token(TokenType::GREATER);
                 }
-            '/'=>{  
-                if(self.matches('/')){
-                    while self.peek()!='\n' && !self.isAtEnd() {
+            }
+            '/' => {
+                if self.matches('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
                         self.advance();
                     }
-                }else{
-                    self.addToken(TokenType::SLASH);
+                } else {
+                    self.add_token(TokenType::SLASH);
                 }
-                }
-            ' '=>{
-
-            } 
-            '\r'=>{
-
             }
-            '\t'=>{
-
+            ' ' => {}
+            '\r' => {}
+            '\t' => {}
+            '\n' => {
+                self.line += 1;
             }
-            '\n'=>{
-                self.line+=1;
-            }
-            '"'=>{
+            '"' => {
                 self.string();
             }
-            'o'=>{
-                if self.peek()=='r' {
-                    self.addToken(TokenType::OR);
+            'o' => {
+                if self.peek() == 'r' {
+                    self.add_token(TokenType::OR);
                 }
             }
             _ => {
-                if(Scanner::isDigit(c)){
+                if Scanner::is_digit(c) {
                     self.number();
-                }else if  Scanner::isAlpha(c){
+                } else if Scanner::is_alpha(c) {
                     self.indentifier();
-                }else{
-                    print!("Unexpected character at line {}",self.line);
+                } else {
+                    print!("Unexpected character at line {}", self.line);
                 }
             }
         }
     }
-    fn isAlphaNumeric(c:char)->bool{
-        Scanner::isAlpha(c) || Scanner::isDigit(c)
+    fn is_alpha_numeric(c: char) -> bool {
+        Scanner::is_alpha(c) || Scanner::is_digit(c)
     }
-    fn isAlpha(c:char)->bool{
-        (c>='a' && c<='z') || (c>='A' && c<='Z') || c=='_'
+    fn is_alpha(c: char) -> bool {
+        (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
     }
-    fn indentifier(&mut self)->(){
-        while Scanner::isAlphaNumeric(self.peek()) {
+    fn indentifier(&mut self) -> () {
+        while Scanner::is_alpha_numeric(self.peek()) {
             self.advance();
         }
-        
+
         let val = String::from_utf8(self.source[self.start..self.current].to_vec()).unwrap();
 
         let token_type: TokenType = match self.keywords.get(&val) {
@@ -196,85 +264,90 @@ impl Scanner {
         };
 
         match token_type {
-            TokenType::IDENTIFIER => self.addTokenLiteral(
-                TokenType::IDENTIFIER,
-                Some(Literal::Indentifier(val)),
-            ), // book doesn't do this. why not?}
-            _ => self.addToken(token_type),
+            TokenType::IDENTIFIER => {
+                self.add_token_literal(TokenType::IDENTIFIER, Some(Literal::Indentifier(val)))
+            } // book doesn't do this. why not?}
+            _ => self.add_token(token_type),
         }
-        
-        self.addToken(TokenType::IDENTIFIER);
+
+        self.add_token(TokenType::IDENTIFIER);
     }
-    fn isDigit(c:char)->bool{
-        c>='0' && c<='9'
+    fn is_digit(c: char) -> bool {
+        c >= '0' && c <= '9'
     }
-    fn number(&mut self)->(){
-        while Scanner::isDigit(self.peek()){
+    fn number(&mut self) -> () {
+        while Scanner::is_digit(self.peek()) {
             self.advance();
         }
-        if(self.peek()=='.'&&Scanner::isDigit(self.peekNext())){
-            self.advance(); 
-            while Scanner::isDigit(self.peek()) {
+        if self.peek() == '.' && Scanner::is_digit(self.peek_next()) {
+            self.advance();
+            while Scanner::is_digit(self.peek()) {
                 self.advance();
             }
         }
         let val: f64 = String::from_utf8(self.source[self.start..self.current].to_vec())
-        .unwrap()
-        .parse()
-        .unwrap();
-        self.addTokenLiteral(TokenType::NUMBER,Some(Literal::Number(val)));
+            .unwrap()
+            .parse()
+            .unwrap();
+        self.add_token_literal(TokenType::NUMBER, Some(Literal::Number(val)));
     }
-    fn peekNext(&mut self)->char{
-        if self.current+1>=self.source.len(){
+    fn peek_next(&mut self) -> char {
+        if self.current + 1 >= self.source.len() {
             return '\0';
         }
-        char::from(self.source[self.current+1])
+        char::from(self.source[self.current + 1])
     }
-    fn string(&mut self) -> (){
-       while self.peek()!='"' && !self.isAtEnd() {
-        if(self.peek()=='\n'){
-            self.line+=1;
+    fn string(&mut self) -> () {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1;
+            }
+            if self.is_at_end() {
+                println!("Unterminated string at line {}", self.line);
+            }
+            self.advance();
+            let value = self.source[self.start + 1..self.current - 1].to_vec();
+            self.add_token_literal(
+                TokenType::STRING,
+                Some(Literal::Str(String::from_utf8(value).unwrap())),
+            );
         }
-        if(self.isAtEnd()){
-            println!("Unterminated string at line {}",self.line);
-        }
-        self.advance();
-        let value = self.source[self.start+1..self.current-1].to_vec();
-        self.addTokenLiteral(TokenType::STRING,Some(Literal::Str(String::from_utf8(value).unwrap())));
-       } 
     }
-    fn peek(&mut self) -> char{
-        if(self.isAtEnd()){
+    fn peek(&mut self) -> char {
+        if self.is_at_end() {
             return '\0';
         }
         return char::from(self.source[self.current]);
     }
-    fn matches(&mut self,expected:char) -> bool{
-        if self.isAtEnd() {
+    fn matches(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
             return false;
-        }   
+        }
         if char::from(self.source[self.current]) != expected {
             return false;
         }
-        self.current+=1;
+        self.current += 1;
         true
     }
-    fn addToken(&mut self, tt: TokenType) -> () {
-        self.addTokenLiteral(tt,None)
+    fn add_token(&mut self, tt: TokenType) -> () {
+        self.add_token_literal(tt, None)
     }
-    fn addTokenLiteral(&mut self, tt: TokenType, literal: Option<Literal>) -> (){
-       let text = self.source[self.start..self.current].to_vec();
-       self.tokens.push(Token { ttype: tt, lexeme: text, literal: literal, line: self.line })
+    fn add_token_literal(&mut self, tt: TokenType, literal: Option<Literal>) -> () {
+        let text = self.source[self.start..self.current].to_vec();
+        self.tokens.push(Token {
+            ttype: tt,
+            lexeme: text,
+            literal: literal,
+            line: self.line,
+        })
     }
     //fn addToken(tt: TokenType, literal: &dyn Any) -> () {
     //    Self::addToken(tt)
     //}
 
-
     fn advance(&mut self) -> char {
-        self.current+=1;
-
-        char::from(self.source[self.current-1])
+        self.current += 1;
+        char::from(self.source[self.current - 1])
         //let my_vec: Vec<char> = self.source.chars().collect();
         //let index = usize::try_from(self.current).unwrap();
         //my_vec[index-1]
